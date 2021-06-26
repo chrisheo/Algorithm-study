7b38be9e06c003c70cae2466cad76dccc6b60de4
YEONUPC\jftj9 <jftj97@gmail.com>
2021-06-26 22:54:08 +0900

    1181_정렬

---
+++ 1181_정렬.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+import sys
+
+l1 = []
+d1 = defaultdict(list)
+for i in range(int(input("숫자를 입력해주세요 :"))):  
+    l1.append(sys.stdin.readline().strip())
+
+l1 = list(set(l1))
+l1.sort()
+num = len(l1)
+for i in range(num):
+    cnt = len(l1[i])
+    d1[cnt].append(l1[i])
+d1 = sorted(d1.items())
+for i in d1:
+    for j in i[1]:
+        print(j)
---
+++ 1181_정렬_다정.py
@@ -0,0 +1,30 @@
+num = int(input("숫자를 입력해주세요 :"))
+lst = []
+for i in range(num):
+    word = input()
+    lst.append(word)
+
+lst_set = set(lst)
+lst.clear()
+for i in lst_set:
+    length = len(i)
+    word_lst = [i, length]
+    lst.append(word_lst)
+
+
+for i in range(0, len(lst)-1):
+    small = i
+    for j in range(i+1, len(lst)):
+        if lst[small][1] > lst[j][1]:
+            small = j
+    if i != small:
+        temp = lst[small]
+        lst[small] = lst[i]
+        lst[i] = temp
+
+
+lst.sort()
+for i in range(0, len(lst)):
+    print(lst[i][0])
+
+
