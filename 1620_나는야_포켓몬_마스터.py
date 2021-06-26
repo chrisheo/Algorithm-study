65a06a6284675fabb635423e931333d3c9b3cf1e
YEONUPC\jftj9 <jftj97@gmail.com>
2021-06-27 00:58:02 +0900

    1620성공 list 인덱싱해서 시간초과.. dictionary로 바꿈

--- 1620_나는야_포켓몬_마스터.py
+++ 1620_나는야_포켓몬_마스터.py
@@ -1,1 +1,19 @@
-print(3)
+import sys
+N, M = input().split()
+N = int(N)
+M = int(M)
+l = []
+d = {}
+for i in range(N):
+    p = sys.stdin.readline().split()
+    l.append(''.join(p))
+    d[''.join(p)] = i + 1
+
+for j in range(M):
+    k = sys.stdin.readline().split()
+    o = ''.join(k)
+    if o.isdigit():
+        o = int(o)
+        print(l[o - 1])
+    else:
+        print(d[o])
