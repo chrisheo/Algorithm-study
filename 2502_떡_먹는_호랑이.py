dd3424c022bbf6c4f93d5d7245aa224874ac8ed9
YEONUPC\jftj9 <jftj97@gmail.com>
2021-07-02 12:21:34 +0900

    update 2502 using recursive function

---
+++ 2502_떡_먹는_호랑이.py
@@ -0,0 +1,21 @@
+d,k = input().split()
+d = int(d)
+k = int(k)
+def rev_pivo(day,first,second):
+    if day == 3:
+        print(first)
+        print(second)
+        return
+    else:
+        temp = first
+        first = second - first
+        second = temp
+        if first > second:
+            return -1
+        return rev_pivo(day - 1,first,second)
+    return -1
+for i in range(int(k/2),0,-1):
+    if rev_pivo(d,i,k-i) != -1:
+        break
+
+    
