1fd79f895ac11b0b43fdca957fa6f5f18f1b2e8e
허연우 <jftj97@gmail.com>
2022-08-04 23:37:10 +0900

    solve leet 1

---
+++ LeetCode/problems/1 Two Sum.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in nums:
+                if nums.index(diff) != i:
+                    return(i, nums.index(diff))
